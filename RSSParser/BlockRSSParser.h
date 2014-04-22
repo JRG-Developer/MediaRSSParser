b6995ab3305881902fe396c35aa4c478e4b53435
Joshua <jrg.developer@gmail.com>
2014-04-22 16:50:47 -0700

    Added `HTML` support categories to project

---
+++ RSSParser/BlockRSSParser.h
@@ -0,0 +1,14 @@
+//
+//  BlockRSSParser.h
+//  RSSParser_Sample
+//
+//  Created by Joshua on 4/2/14.
+//
+//
+
+#import <BlockRSSParser/RSSItem.h>
+#import <BlockRSSParser/RSSParser.h>
+#import <BlockRSSParser/RSSMediaCredit.h>
+#import <BlockRSSParser/RSSMediaThumbnail.h>
+#import <BlockRSSParser/GTMNSString+HTML.h>
+#import <BlockRSSParser/NSString+HTML.h>
